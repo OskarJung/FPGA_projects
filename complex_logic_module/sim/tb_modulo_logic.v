afe8819f7b8b09e51f770b7ad732a393387432e8
Oskar <175014126+OskarJung@users.noreply.github.com>
2026-05-06 11:58:03 +0200

    Translate comments in tb_modulo_logic.v to English

--- complex_logic_module/sim/tb_modulo_logic.v
+++ complex_logic_module/sim/tb_modulo_logic.v
@@ -34,41 +34,41 @@ module tb_modulo_logic;
 
     initial begin
 
-        // Wektor 1: Wszystkie 0. Oczekiwany wynik: 0
+         // vec 1: All zeros. Expected result: 0
         x_tb = 8'h00; y_tb = 8'h00; 
         #10; 
 
-        // Wektor 2: Wszystkie 1. Oczekiwany wynik: 1
+        // vec 2: All ones. Expected result: 1
         x_tb = 8'hFF; y_tb = 8'hFF; 
         #10;
 
-        // Wektor 3:  'x' to 1, ale 'y' to 0. 
-        // Oczekiwany wynik: 0
+        // vec 3: 'x' is 1, but 'y' is 0. 
+        // Expected result: 0
         x_tb = 8'hFF; y_tb = 8'h00; 
         #10;
 
-        // Wektor 4: Aktywacja tylko x[0] i y[0]
-        // Oczekiwany wynik: 0.
+        // vec 4: Activate only x[0] and y[0]
+        // Expected result: 0
         x_tb = 8'b00000001; y_tb = 8'b00000001; 
         #10;
 
-        // Wektor 5: Aktywacja x[0] i y[0], x[1] i y[2].
-        // Oczekiwany wynik: 0.
+        // vec 5: Activate x[0] and y[0], x[1] and y[2]
+        // Expected result: 0
         x_tb = 8'b00000011; y_tb = 8'b00000011; 
         #10;
 
-        // Wektor 6: Aktywacja x[0] i y[0], x[1] i y[2], x[3] i y[3].
-        // Oczekiwany wynik: 1.
+        // vec 6: Activate x[0] and y[0], x[1] and y[2], x[3] and y[3]
+        // Expected result: 1
         x_tb = 8'b00000111; y_tb = 8'b00000111; 
         #10;
 
-        // Wektor 7: Aktywacja prawej polowy drzewa.
-        // Oczekiwany wynik: 1.
+        // vec 7: Activate the right half of the tree
+        // Expected result: 1
         x_tb = 8'b00001111; y_tb = 8'b00001111; 
         #10;
 
-        // Wektor 8: Aktywacja co drugiego bitu (bity 1, 3, 5, 7).
-        // Oczekiwany wynik: 1.
+        // vec 8: Activate every second bit (bits 1, 3, 5, 7)
+        // Expected result: 1
         x_tb = 8'b01010101; y_tb = 8'b01010101; 
         #10;
 
