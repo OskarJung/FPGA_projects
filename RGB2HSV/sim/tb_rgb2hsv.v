13fb1bc36406ef5192768c83c263986401debb16
Oskar <175014126+OskarJung@users.noreply.github.com>
2026-05-17 17:27:09 +0200

    Add testbench for RGB to HSV conversion module

---
+++ RGB2HSV/sim/tb_rgb2hsv.v
@@ -0,0 +1,89 @@
+`timescale 1ns / 1ps
+
+module tb_rgb2hsv();
+    // inputs
+    reg clk = 1'b0;
+    reg [23:0] pixel_in;
+    reg de_in;
+    reg hsync_in;
+    reg vsync_in;
+
+    // outputs
+    wire [23:0] pixel_out;
+    wire de_out;
+    wire hsync_out;
+    wire vsync_out;   
+    
+    rgb2hsv dut (
+        .clk(clk),
+        .pixel_in(pixel_in),
+        .de_in(de_in),
+        .hsync_in(hsync_in),
+        .vsync_in(vsync_in),
+        .pixel_out(pixel_out),
+        .de_out(de_out),
+        .hsync_out(hsync_out),
+        .vsync_out(vsync_out)
+    );
+
+    wire [7:0] sim_H  = pixel_out[23:16];
+    wire [7:0] sim_S = pixel_out[15:8];
+    wire [7:0] sim_V = pixel_out[7:0]; 
+    
+    // Clock generation
+    always #1 clk = ~clk;
+
+    initial begin
+        pixel_in = 24'd0;
+        de_in    = 0;
+        hsync_in = 0;
+        vsync_in = 0;
+        
+        $display("\n================ start simulation ================\n");
+        
+        #20; 
+        
+        // Test 1: black (R=255, G=100, B=190)
+        //expected 0, 128, 128
+        @(negedge clk);
+        pixel_in = {8'd255, 8'd100, 8'd190}; 
+        de_in    = 1;
+        hsync_in = 1;
+
+        // Test 2: white (R=120, G=120, B=120)
+        @(negedge clk);
+        pixel_in = {8'd120, 8'd120, 8'd120}; 
+        hsync_in = 0;
+
+        // Test 3: red (R=50, G=200, B=100)
+        @(negedge clk);
+        pixel_in = {8'd50, 8'd200, 8'd100}; 
+
+        // Test 4: 
+        @(negedge clk);
+        pixel_in = {8'd10, 8'd50, 8'd250}; 
+
+        // Test 5: matlab (R=255, G=100, B=123)
+        @(negedge clk);
+        pixel_in = {8'd255, 8'd0, 8'd0}; 
+
+        // End of valid data stream
+        @(negedge clk);
+        de_in    = 0;
+        pixel_in = 24'd0;
+
+        // Wait for pipeline flush (9 takt w latencji w module)
+        #150; 
+        $display("\n================ END Simulation =====================\n");
+        $finish;
+    end
+
+    // Monitor block - verifying outputs independently
+    always @(posedge clk) begin
+        if (de_out) begin
+//            $display("Time: %0t | H = %d, S = %d, V = %d", 
+//                     $time, sim_H, sim_S, sim_V);
+        end
+    end
+
+endmodule
