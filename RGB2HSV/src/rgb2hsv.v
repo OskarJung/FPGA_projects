5b586847f1e417285c6afaa05964c2b6ccdcec71
Oskar <175014126+OskarJung@users.noreply.github.com>
2026-05-17 17:45:31 +0200

    Implement min and max calculations for RGB values and integrate C calculation in rgb2hsv module

--- RGB2HSV/src/rgb2hsv.v
+++ RGB2HSV/src/rgb2hsv.v
@@ -81,6 +81,89 @@ wire de_after_div;
     assign b_01[8] = b_d_255[8]; // czesc calkowita wyniku
     assign b_01[7:0] = b_d_255[7:0]; // czesc ulamkowa wyniku
     
+//===================
+//calculate min and max and C
+//===================  
+
+    wire [9:0] MAX;
+    wire [9:0] MIN;
+    wire [1:0] MAX_idx;
+    wire [1:0] MIN_idx;
+    
+    wire signed [30:0] rgb_01_d1;
+    
+    Delay_Line #(.N(30), .DELAY(1)) delay_rgb_01 (
+        .clk(clk), 
+        .ce(1'b1), 
+        .idata({de_after_div, r_01, g_01, b_01}), 
+        .odata(rgb_01_d1)
+    );
+    wire de_after_min_max = rgb_01_d1[30];
+    wire signed [9:0] r_01_d1 = rgb_01_d1[29:20];
+    wire signed [9:0] g_01_d1 = rgb_01_d1[19:10];
+    wire signed [9:0] b_01_d1 = rgb_01_d1[9:0];
+    
+    //latency #1
+    max_rgb #(.N(10))
+        max (
+            .clk(clk),
+            .ce(de_after_div),
+            .R(r_01),
+            .G(g_01),
+            .B(b_01),
+            .MAX(MAX),
+            .MAX_idx(MAX_idx)
+         );
+    
+    //latency #1
+    min_rgb #(.N(10))
+        min (
+            .clk(clk),
+            .ce(de_after_div),
+            .R(r_01),
+            .G(g_01),
+            .B(b_01),
+            .MIN(MIN),
+            .MIN_idx(MIN_idx)
+         );  
+            
+        wire signed [9:0] C_01; 
+        
+        //latency 2
+        C_substracter_L2 C_sub (
+          .A(MAX),      // input wire [9:0] A
+          .B(MIN),      // input wire [9 : 0] B
+          .CLK(clk),  // input wire CLK
+          .CE(de_after_min_max),    // input wire CE
+          .S(C_01)      // output wire [9 : 0] S
+        );
+        
+        wire de_after_C;
+        Delay_Line #(.N(30), .DELAY(1)) delay_after_C (
+            .clk(clk), 
+            .ce(1'b1), 
+            .idata(de_after_min_max), 
+            .odata(de_after_C)
+        );         
+        
+//===========================
+// Calculate S        
+// ==========================        
+    wire signed [23:0] S_reg;
+    div_S_L22 div_S (
+      .aclk(clk),                    // input wire aclk
+      .s_axis_divisor_tvalid(de_after_C),    // input wire s_axis_divisor_tvalid
+      .s_axis_divisor_tdata(C_01),      // input wire [15 : 0] s_axis_divisor_tdata
+      .s_axis_dividend_tvalid(de_after_min_max),  // input wire s_axis_dividend_tvalid
+      .s_axis_dividend_tdata(MAX),    // input wire [15 : 0] s_axis_dividend_tdata
+      .m_axis_dout_tvalid(de_after_2D),          // output wire m_axis_dout_tvalid
+      .m_axis_dout_tdata(S_reg)            // output wire [23 : 0] m_axis_dout_tdata
+);
+
+    wire signed [9:0] S_01; 
+    assign S_01[9] = S_reg[23];
+    assign b_01[8] = S_reg[8]; // czesc calkowita wyniku
+    assign b_01[7:0] = S_reg[7:0]; // czesc ulamkowa wyniku
         
 // ==================================
 // delay synchronization sygnal and final assigment
@@ -89,7 +172,7 @@ wire de_after_div;
     wire [2:0] sync_in  = {de_in, hsync_in, vsync_in};
     wire [2:0] sync_out;
     
-    Delay_Line #(.N(3), .DELAY(18)) delay_sync (
+    Delay_Line #(.N(3), .DELAY(19)) delay_sync (
         .clk(clk), 
         .ce(1'b1), 
         .idata(sync_in), 
